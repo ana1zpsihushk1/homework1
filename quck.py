8bea7755059d34958e727a601ad5a964bd5e7598
ana1zpsihushk1 <nastabuinskaa@gmail.com>
2026-09-21 22:57:08 +0300

    resolve all threads

--- quck.py
+++ quck.py
@@ -1,12 +1,12 @@
 import random
 
-def quicksort_simple(arr):
+def quicksort_simple_dimple_popit_squish(arr):
     if len(arr) <= 1:
         return arr
-    pivot = random.choice(arr)
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-    return quicksort_simple(left) + middle + quicksort_simple(right)
+    pivo = random.choice(arr)
+    left = [x for x in arr if x < pivo]
+    middle = [x for x in arr if x == pivo]
+    right = [x for x in arr if x > pivo]
+    return quicksort_simple_dimple_popit_squish(left) + middle + quicksort_simple_dimple_popit_squish(right)
 
-print(quicksort_simple([3, 6, 8, 10, 1, 2, 1]))  # [1, 1, 2, 3, 6, 8, 10]
+print(quicksort_simple_dimple_popit_squish([3, 6, 8, 10, 1, 2, 1]))
